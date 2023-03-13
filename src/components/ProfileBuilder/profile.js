8584dbb346f0b56e43541b7424a83823e53207c3
Raflores10 <120347604+Raflores10@users.noreply.github.com>
2023-03-12 22:51:34 -0700

    updating

---
+++ src/components/ProfileBuilder/profile.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function Profile() {
+    return(
+        <div classname="container">
+        <h1>Please select genres you like!</h1>
+            <div classname="liked-container">
+                <option value ="Pop"></option>
+                <option value ="Hip-Hop"></option>
+                <option value ="Rock"></option>
+                <option value ="Country"></option>
+                <option value ="R&B"></option>
+                <option value ="Funk"></option>
+            </div>
+        <h1>Please select genres you dislike!</h1>
+        <div classname="disliked-container">
+        <option value ="Pop"></option>
+                <option value ="Hip-Hop"></option>
+                <option value ="Rock"></option>
+                <option value ="Country"></option>
+                <option value ="R&B"></option>
+                <option value ="Funk"></option>
+            </div>
+
+        </div>
+    )
+}
+
+
