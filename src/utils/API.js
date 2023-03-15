ec2f6b7723ad2bb25b863b6d0f314f811c8636d6
Raflores10 <120347604+Raflores10@users.noreply.github.com>
2023-03-15 13:43:37 -0700

    fixing merge issue

--- src/utils/API.js
+++ src/utils/API.js
@@ -5,72 +5,80 @@ const baseUrl = "http://localhost:3001"
 // const baseUrl = "https://be-808.herokuapp.com
 
 const API = {
-//gets all genres and they appear in the console (verified)
-    getAllGenres : ()=> {
-
-        // return fetch(`https://be-808.herokuapp.com/api/genres`)
-        return fetch(`https://localhost:3001/api/genres`)
+    //gets all genres and they appear in the console (verified)
+    getAllGenres: () => {
 
 
-        return fetch(`${baseUrl}/api/genres`)
+        return fetch(`${baseUrl}/api/genres`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
 
-        .then(res=>res.json())
+            .then(res => res.json())
     },
 
-    getUserData:id=>{
+    getUserData: id => {
 
         // return fetch(`https://be-808.herokuapp.com/api/users/${id}`)
-        return fetch(`https://localhost:3001/api/users/${id}`)
-        .then(res=>res.json())
+        return fetch(`https://localhost:3001/api/users/${id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(res => res.json())
     },
 
-    getAllData : () => {
-        // return fetch(`https://be-808.herokuapp.com/api/users/`)
-        return fetch(`https://localhost:3001/api/users/`)
+    getAllData: () => {
 
 
-        return fetch(`${baseUrl}/api/users/${id}`)
-        .then(res=>res.json())
+
+        return fetch(`${baseUrl}/api/users/${id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(res => res.json())
     },
 
-    login:userObj=>{
-        // return fetch(`https://be-808.herokuapp.com/api/users/login`,{ 
-        return fetch(`https://localhost:3001/api/users/login`,{ 
+    login: userObj => {
 
-        return fetch(`${baseUrl}/api/users/login`,{ 
-        method: "POST",
-        body:JSON.stringify(userObj),
-        headers:{
-            "Content-Type":"application/json"
-        }
-    }).then(res=>res.json())
+        return fetch(`${baseUrl}/api/users/login`, {
+            method: "POST",
+            body: JSON.stringify(userObj),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(res => res.json())
     },
 
-    signup:userObj=>{
-        // return fetch(`https://be-808.herokuapp.com/api/users`,{ 
-        return fetch(`https://localhost:3001/api/users`,{ 
 
 
-        return fetch(`${baseUrl}/api/users`,{ 
-        method: "POST",
-        body:JSON.stringify(userObj),
-        headers:{
-            "Content-Type":"application/json"
-        }
-    }).then(res=>res.json())
-    }, 
-    
-   isValidToken:(token) => {
-    return fetch(`${baseUrl}/api/users/isValidToken`,{ 
-        method: "GET",
-        headers:{
-            "Content-Type":"application/json",
-            "authorization":`Bearer ${token}`
-        
-        }
-    }).then(res=>res.json())
-   }
+
+    signup: (userObj) => {
 
 
+        return fetch(`${baseUrl}/api/users`, {
+            method: "POST",
+            body: JSON.stringify(userObj),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(res => res.json())
+    },
+
+    isValidToken: (token) => {
+        return fetch(`${baseUrl}/api/users/isValidToken`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "authorization": `Bearer ${token}`
+
+            }
+        }).then(res => res.json())
+    },
 }
 export default API
